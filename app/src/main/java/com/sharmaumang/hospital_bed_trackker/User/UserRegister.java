19bd2006374271627d0971bda3f928ebe71e4f5b
sharmaumang001 <itsumangsharma@gmail.com>
2020-12-11 15:54:52 +0530

    User Files and Activities added

---
+++ app/src/main/java/com/sharmaumang/hospital_bed_trackker/User/UserRegister.java
@@ -0,0 +1,17 @@
+package com.sharmaumang.hospital_bed_trackker.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sharmaumang.hospital_bed_trackker.R;
+
+
+public class UserRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+    }
+}
